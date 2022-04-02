9a7d759f20ed2c16dff43777f33421044f2e1f63
Sumin <cjstk4_@naver.com>
2022-04-02 17:42:30 +0900

    0402

--- Project/src/inMemory/Util.java
+++ Project/src/inMemory/Util.java
@@ -1,6 +1,7 @@
 package inMemory;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -96,7 +97,52 @@ public class Util {
 		return result;
 	}
 	
-	// 계정
+	public static ArrayList<Account> fileToAccount(String path) {
+		File f = new File(path);
+		if(!f.exists()) {
+			System.err.println("파일이 존재하지 않습니다.");
+			return null;
+		}
+		
+		ArrayList<Account> returnArray = new ArrayList<>();
+		ArrayList<String> arrayFile = readFileLine(path);
+		
+		for(int i=0; i<arrayFile.size(); i++) {
+			String[] split = arrayFile.get(i).split(",");
+			String name = split[0];
+			String birthDate = split[1];
+			String cellphone = split[2];
+			String id = split[3];
+			String password = split[4];
+			returnArray.add(new Account(name, birthDate, cellphone, id, password));
+		}
+		
+		return returnArray;
+	}
+	
+	public static ArrayList<Books> fileToBooks(String path) {
+		File f = new File(path);
+		if(!f.exists()) {
+			System.err.println("파일이 존재하지 않습니다.");
+			return null;
+		}
+		
+		ArrayList<Books> returnArray = new ArrayList<>();
+		ArrayList<String> arrayFile = readFileLine(path);
+		
+		for(int i=0; i<arrayFile.size(); i++) {
+			String[] split = arrayFile.get(i).split(",");
+			String title = split[0];
+			String author = split[1];
+			String publisher = split[2];
+			String borrower = split[3];
+			String rentalDate = split[4];
+			String returnDate = split[5];
+			returnArray.add(new Books(title, author, publisher, borrower, rentalDate, returnDate));
+		}
+		
+		return returnArray;
+	}
 	
 
 }
